dfb7fac9d4ba164df3a617cddbcad7d59e513e37
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-04 17:56:32 -0600

    Add ActiveRecord query to question model.

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,11 @@ class Question < ApplicationRecord
   has_many :questionnaires, through: :questionnaire_questions
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def self.find_demographics(tags)
+    select(:id, :text)
+    .joins(:tags)
+    .where('tags.id = any (array[?])', tags)
+    .where(section_id: 1)
+  end
 end
