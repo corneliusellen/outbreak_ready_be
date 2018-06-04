8d85368a03f821390bdf5b3d896b1f82b4cebe49
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-04 17:56:07 -0600

    Add tag association to questionnaire.

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -2,4 +2,6 @@ class Questionnaire < ApplicationRecord
   validates_presence_of :title
   has_many :questionnaire_questions
   has_many :questions, through: :questionnaire_questions
+  has_many :intakes
+  has_many :tags, through: :intakes
 end
