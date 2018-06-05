1d4c5192f6ec232610dd29912590355a78e989b6
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-04 20:28:58 -0600

    Create index action for exposures controller.

--- app/controllers/api/v1/exposures_controller.rb
+++ app/controllers/api/v1/exposures_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::ExposuresController < ApplicationController
 
   def index
-
+    questionnaire = Questionnaire.find(params[:questionnaire_id])
+    tags = questionnaire.tags.map {|tag| tag.id}
+    tags.push(25)
+    render json: Question.find_exposures(tags)
   end
 end
