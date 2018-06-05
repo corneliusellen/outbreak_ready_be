e4453365a0d5ef6a0ac435765cfb0b294e36db85
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-05 14:24:12 -0600

    Change menu endpoint from parameters to headers to fit front-end.

--- spec/requests/api/v1/sends_menus_spec.rb
+++ spec/requests/api/v1/sends_menus_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'Search API' do
   it 'Sends menus for specified restaurant' do
-    params = {venue_id: '4dc2d699d22dd7df9ab25063'}
-    get "/api/v1/search/menus", params: params
+    headers = { 'CONTENT-TYPE' => 'application/json', 'HTTP_VENUE_ID' => '4dc2d699d22dd7df9ab25063' }
+    get "/api/v1/search/menus", headers: headers
 
     result = JSON.parse(response.body)
 
