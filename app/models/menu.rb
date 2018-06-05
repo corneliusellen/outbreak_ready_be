0d243c90f92785208a91826c23042c17432a539b
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-05 14:23:29 -0600

    Add venue id to json.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,8 +1,9 @@
 class Menu
   include ActiveModel::Serialization
-  attr_reader :name, :data, :foods
+  attr_reader :id, :name, :data, :foods
 
   def initialize(info)
+    @id = info["menuId"]
     @name = info["name"]
     @data = info["entries"]["items"]
     @foods = []
