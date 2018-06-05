2aee7501c374b2c7085afc2aa6538282702d8971
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-04 20:29:56 -0600

    Change http header.

--- app/controllers/api/v1/questionnaires_controller.rb
+++ app/controllers/api/v1/questionnaires_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::QuestionnairesController < ApplicationController
 
   def update
     questionnaire = Questionnaire.find(params[:id])
-    JSON.parse(request.env['HTTP_DEMOGRAPHICS']).each do |question_id|
+    JSON.parse(request.env['HTTP_QUESTIONS']).each do |question_id|
       QuestionnaireQuestion.create(questionnaire_id: questionnaire.id, question_id: question_id.to_i)
     end
   end
--- spec/requests/api/v1/saves_demographic_qq_spec.rb
+++ spec/requests/api/v1/saves_demographic_qq_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Questionnnaire API' do
   it "saves demographic questionnaire questions" do
-    headers = { "CONTENT-TYPE" => "application/json", "HTTP_DEMOGRAPHICS" => "[\"15\",\"16\",\"67\",\"65\"]" }
+    headers = { "CONTENT-TYPE" => "application/json", "HTTP_QUESTIONS" => "[\"15\",\"16\",\"67\",\"65\"]" }
 
     put '/api/v1/questionnaires/1', :headers => headers
 
