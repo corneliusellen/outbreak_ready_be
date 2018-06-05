c39ad1f04f6021ce6bfa470edc1fcb069365cf66
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-04 18:50:52 -0600

    Create patch action for questionnaire controller.

--- app/controllers/api/v1/questionnaires_controller.rb
+++ app/controllers/api/v1/questionnaires_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::QuestionnairesController < ApplicationController
   def create
     Questionnaire.create!(title: "Test 2")
   end
+
+  def update
+    questionnaire = Questionnaire.find(params[:id])
+    JSON.parse(request.env['HTTP_DEMOGRAPHICS']).each do |question_id|
+      QuestionnaireQuestion.create(questionnaire_id: questionnaire.id, question_id: question_id.to_i)
+    end
+  end
 end
