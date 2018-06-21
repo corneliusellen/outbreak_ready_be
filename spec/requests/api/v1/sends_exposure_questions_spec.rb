553b3ca1b9e7fd2036c337a9141e19986cf0077a
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-21 13:45:14 -0600

    Modify spec for sending exposure questions.

--- spec/requests/api/v1/sends_exposure_questions_spec.rb
+++ spec/requests/api/v1/sends_exposure_questions_spec.rb
@@ -4,6 +4,17 @@ describe 'Questionnnaire API' do
   it "sends exposure questions" do
     Intake.create(questionnaire_id: 1, tag_id: 16)
     Intake.create(questionnaire_id: 1, tag_id: 7)
+
+    get '/api/v1/questionnaires/1/exposures'
+
+    result = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(result.count).to eq(31)
+  end
+
+  it "ensures questions are sent only with all tags present" do
+    Intake.create(questionnaire_id: 1, tag_id: 16)
     Intake.create(questionnaire_id: 1, tag_id: 20)
 
     get '/api/v1/questionnaires/1/exposures'
@@ -11,12 +22,11 @@ describe 'Questionnnaire API' do
     result = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(result.count).to eq(20)
+    expect(result.count).to eq(6)
   end
 
-  it "ensures questions are sent only with all tags present" do
-    Intake.create(questionnaire_id: 1, tag_id: 16)
-    Intake.create(questionnaire_id: 1, tag_id: 20)
+  it "ensures questions are sent with OR tags present" do
+    Intake.create(questionnaire_id: 1, tag_id: 14)
 
     get '/api/v1/questionnaires/1/exposures'
 
