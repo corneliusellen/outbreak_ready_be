7c5a2eb115a90ce98102dc6517be4ced6c6a5ef8
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-21 14:10:48 -0600

    Refactor by moving logic from exposure controller to appliation controller and using before actions.

--- app/controllers/api/v1/exposures_controller.rb
+++ app/controllers/api/v1/exposures_controller.rb
@@ -1,29 +1,10 @@
 class Api::V1::ExposuresController < ApplicationController
-
-  def index
-    questionnaire = Questionnaire.find(params[:questionnaire_id])
-    @tags = questionnaire.tags.map {|tag| tag.id}
-    @tags.push(25)
-    questions = Question.find_exposures
-    filtered = questions.find_all do |question|
-      tags = question.all_tags
-      classifications = question.all_classifications
-      taggings = tags.zip(classifications)
-      check_included(taggings)
-    end
-    render json: filtered
+  before_action :identify_questionnaire_tags
+  before_action  do
+    filter_questions(3)
   end
 
-  def check_included(taggings)
-    hash = {1 => [], 2 => [], 3 => []}
-    taggings.each do |element|
-      hash[element[1]] += [element[0]]
-    end
-    hash.delete_if {|k,v| v.empty?}
-    hash.all? do |tag|
-      @tags.include?(tag[1][0]) ||
-      @tags.include?(tag[1][1]) ||
-      @tags.include?(tag[1][2])
-    end
+  def index
+    render json: @filtered
   end
 end
