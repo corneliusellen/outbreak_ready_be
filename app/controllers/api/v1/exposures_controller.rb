ec96c2bf1311659eb4f9eff9c5122719699c9249
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-21 13:46:20 -0600

    Add logic for filtering tags based on classification. Refactor to model.

--- app/controllers/api/v1/exposures_controller.rb
+++ app/controllers/api/v1/exposures_controller.rb
@@ -3,17 +3,27 @@ class Api::V1::ExposuresController < ApplicationController
   def index
     questionnaire = Questionnaire.find(params[:questionnaire_id])
     @tags = questionnaire.tags.map {|tag| tag.id}
-    # tags.push(25)
+    @tags.push(25)
     questions = Question.find_exposures
     filtered = questions.find_all do |question|
-      check_included(question.all_tags)
+      tags = question.all_tags
+      classifications = question.all_classifications
+      taggings = tags.zip(classifications)
+      check_included(taggings)
     end
     render json: filtered
   end
 
-  def check_included(all_tags)
-    all_tags.all? do |tag|
-      @tags.include?(tag)
+  def check_included(taggings)
+    hash = {1 => [], 2 => [], 3 => []}
+    taggings.each do |element|
+      hash[element[1]] += [element[0]]
+    end
+    hash.delete_if {|k,v| v.empty?}
+    hash.all? do |tag|
+      @tags.include?(tag[1][0]) ||
+      @tags.include?(tag[1][1]) ||
+      @tags.include?(tag[1][2])
     end
   end
 end
