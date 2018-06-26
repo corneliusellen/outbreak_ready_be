817833b220dfca3805cc873f6e809f9003925d3b
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-26 09:18:42 -0600

    Reoragnize routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'authenticate', to: 'authentication#authenticate'
+      get '/tags', to: 'tags#index'
+
       resources :questionnaires do
         resources :intakes, only: [:create]
+        post '/foods', to: 'foods#create'
         get '/demographics', to: 'demographics#index'
         get '/clinicals', to: 'clinicals#index'
         get '/exposures', to: 'exposures#index'
         get '/questions', to: 'questions#index'
         get '/foods', to: 'foods#index'
-        post '/foods', to: 'foods#create'
       end
-      get '/tags', to: 'tags#index'
+      
       namespace :search do
         get '/restaurants', to: 'restaurants#index'
         get '/menus', to: 'menus#index'
